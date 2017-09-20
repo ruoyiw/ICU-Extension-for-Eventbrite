74fcc4cf83f751670db533a924ab38f7f75471c5
Zhijin Li <qinqinlizhi1993@163.com>
2017-09-20 17:24:05 +1000

    add new default template

--- UI/js/template.js
+++ UI/js/template.js
@@ -31,11 +31,28 @@ var SAMPLE_SVG_STRING = '<svg width="580" height="400" xmlns="http://www.w3.org/
     '  <path id="svg_1" d="m186.5825,162.04691l79.06753,0l24.43247,-73.71964l24.43249,73.71964l79.06751,0l-63.96685,45.56072l24.43374,73.71964l-63.96688,-45.56197l-63.96687,45.56197l24.43374,-73.71964l-63.96688,-45.56072z" stroke-width="1.5" stroke="#000" fill="#ff0000"/>\n' +
     ' </g>\n' +
     '</svg>'
-
+var SAMPLE_SVG_STRING1 = '<svg width="580" height="400" xmlns="http://www.w3.org/2000/svg">\n'+
+ '<!-- Created with SVG Editor - http://github.com/mzalive/SVG Editor/ -->\n'+
+ '<g>\n'+
+  '<title>background</title>\n'+
+  '<rect fill="#fff" id="canvas_background" height="402" width="582" y="-1" x="-1"/>\n'+
+ '</g>\n'+
+ '<g>\n'+
+  '<title>Layer 1</title>\n'+
+  '<text stroke="#000" transform="matrix(1.2243140935897827,0,0,1.2243140935897827,-44.75066167116165,-12.021833453327417) " xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_1" y="65.39859" x="179.0804" stroke-width="0" fill="#000000">ICU Certificate</text>\n'+
+  '<text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_2" y="131.2" x="209.5" stroke-width="0" stroke="#000" fill="#000000">Awarded to</text>\n'+
+  '<text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_3" y="189.2" x="182.5" stroke-width="0" stroke="#000" fill="#000000">{{studentName}}</text>\n'+
+  '<text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_4" y="242.2" x="252.5" stroke-width="0" stroke="#000" fill="#000000">By</text>\n'+
+  '<text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_5" y="288.2" x="179.5" stroke-width="0" stroke="#000" fill="#000000">{{providerName}}</text>\n'+
+  '<text xml:space="preserve" text-anchor="start" font-family="Helvetica, Arial, sans-serif" font-size="24" id="svg_6" y="337.2" x="443.5" stroke-width="0" stroke="#000" fill="#000000">{{date}}</text>\n'+
+  '<line stroke-linecap="undefined" stroke-linejoin="undefined" id="svg_7" y2="87.2" x2="548.80848" y1="87.2" x1="23.5" stroke-width="1.5" stroke="#000" fill="none"/>\n'+
+  '<rect id="svg_10" height="11" width="37" y="45.2" x="388.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" stroke="#000" fill="#ff0000"/>\n'+
+  '<rect transform="rotate(-0.6500758528709412 407,50.70000076293905) " stroke="#000" id="svg_11" height="39" width="11" y="31.2" x="401.5" fill-opacity="null" stroke-opacity="null" stroke-width="1.5" fill="#ff0000"/>\n'+
+ '</g>\n'+
+'</svg>'
 //TO DO: In the future, obj will be the json Object fetched through ajax-*9+9
 var json_obj = [{"name": "item_1", "content": SAMPLE_SVG_STRING}, 
-                {"name": "item_2", "content": SAMPLE_SVG_STRING},
-                {"name": "item_3", "content": SAMPLE_SVG_STRING}];
+                {"name": "item_2", "content": SAMPLE_SVG_STRING1},];
 
 function init_embed() {
       svgCanvas_singleton.getInstance();
